906c56cb23d92888628a1e59eac53752203c457e
Terciobc <tercio.brandao@newm.com.br>
2024-06-20 14:15:51 -0300

    Initial Commit

---
+++ RandomImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomImage
+//
+//  Created by Tercio Brandão Cavalcanti on 20/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
